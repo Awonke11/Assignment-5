d6767d6be124b87190feb8df9b40e6c87fcdb082
Awonke11 <93478189+Awonke11@users.noreply.github.com>
2023-05-05 20:02:51 +0200

    Update

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # CSC2001F 2023 Assignment 5
-#### Instructions
+### Instructions
 The goal of this assignment is to programmatically compare the performance of Dijkstra's shortest paths algorithm (as per the course slides) with the theoretical performance bounds.
-#### Dataset Generation and Basic Operation
+### Dataset Generation and Basic Operation
 Write an application to generate data, insert the data into a graph and measure the performance experimentally.  Use the attached code (from the textbook) as a starting point (or write everything from scratch or use standard code from elsewhere).
 
 For a single test, you need to assume values for |V| (number of vertices) and |E| (number of edges), then generate a random dataset (in the same format as Graph1.txt).  Save the dataset to a file on disk (so the experiment can be validated later).  Then, load the data into a graph and run Dijkstra's algorithm to determine shortest paths.
--- test/Main.java
+++ test/Main.java
@@ -3,11 +3,28 @@ package test;
 public class Main {
     public static void main(String[] args) {
 
-        TestDirected testDirected = new TestDirected(4);
+        TestDirected testDirected = new TestDirected(20);
         testDirected.showTraversal();
         System.out.println(testDirected.showPerfomance());
 
-        // TestUndirected testUndirected = new TestUndirected(21);
-        // testUndirected.showTraversal();
+        TestDirected testDirected2 = new TestDirected(5);
+        testDirected2.showTraversal();
+        System.out.println(testDirected2.showPerfomance());
+
+        TestDirected testDirected3 = new TestDirected(1);
+        testDirected3.showTraversal();
+        System.out.println(testDirected3.showPerfomance());
+
+        TestUndirected testUndirected = new TestUndirected(20);
+        testUndirected.showTraversal();
+        System.out.println(testUndirected.showPerfomance());
+
+        TestUndirected testUndirected2 = new TestUndirected(5);
+        testUndirected2.showTraversal();
+        System.out.println(testUndirected2.showPerfomance());
+
+        TestUndirected testUndirected3 = new TestUndirected(1);
+        testUndirected3.showTraversal();
+        System.out.println(testUndirected3.showPerfomance());
     }
 }
